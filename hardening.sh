8d564f0cd9607fbf727c6d906ae4720dcceabe90
Vincenzo Bonaiuto <105960233+VincEngineer@users.noreply.github.com>
2023-04-30 16:38:20 +0100

    Update hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -1,9 +1,14 @@
-# Prompt the user to create a Docker user account
-echo -e "\e[33mDo you want to create a Docker user account? [Y/N]:\e[0m"
-read docker_account_choice
+	# Install Docker
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh || { echo -e "\e[31mError installing Docker. Exiting.\e[0m"; exit 1; }
+	# Install Docker Compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
 
-if [[ $docker_account_choice =~ ^[Yy]$ ]]; then
-    echo -e "\e[33m [Hardening] Please create the dockeruser account:\e[0m"
+
+
+
+    echo -e "\e[33m Please create the dockeruser account:\e[0m"
     echo -e "\e[33m Enter your Username:\e[0m"
     read username
     echo -e "\e[33m Enter your Password:\e[0m"
@@ -21,15 +26,6 @@ if [[ $docker_account_choice =~ ^[Yy]$ ]]; then
             docker_user_password+="$char"
         fi
     done
-	# Install Docker
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh get-docker.sh || { echo -e "\e[31mError installing Docker. Exiting.\e[0m"; exit 1; }
-
-
-
-	# Install Docker Compose
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
 
     echo
     sudo useradd -m -s /bin/bash $username
@@ -37,5 +33,3 @@ if [[ $docker_account_choice =~ ^[Yy]$ ]]; then
     sudo usermod -aG docker $username
     sudo usermod -aG sudo $username
 	su - $username
-	whoami
-fi
